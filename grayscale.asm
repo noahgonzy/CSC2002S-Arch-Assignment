69a2a460280ec8d4f324171596d7f96dcbb72089
noahgonzy <GNSNOA001@myuct.ac.za>
2023-09-26 09:41:53 +0200

    commenting on greyscale file added, must try implement changes as cant be tested on mac

--- grayscale.asm
+++ grayscale.asm
@@ -21,12 +21,16 @@
 
 # Open file, read lines, and print them
 main:
+    # FIGURE OUT WRITING SYSTEM, WHICH CALL IS NECESSARY!!!!!!!
     li   $v0, 13       # system call for open file
     la   $a0, filenamewrite     # output file name
-    li   $a1, 'W'        # Open for writing (flags are 0: read, W/A: write)
+    li   $a1, 'W'        # Open for writing to create file
     syscall
 
-    move $t0, $v0 
+    move $t0, $v0   #store file descriptor
+
+    #show error if there's an error creating the file
+    ble $v0, $zero, erro
 
     li $v0, 16               # Syscall code for close file
     move $a0, $t0            # File descriptor to close
@@ -37,8 +41,8 @@ main:
     la $a0, filenameread         # Load the address of the filename
     li $a1, 0                # Open for reading
     syscall
-    li $a0, 0
-
+    
+    #show error if there's an error reading
     ble $v0, $zero, error
 
     move $t0, $v0            # Store the file descriptor in $t0
@@ -65,24 +69,28 @@ read_loop:
     li $v0, 14               # Syscall code for read from file
     move $a0, $t0            # File descriptor
     la $a1, buffer           # Buffer to store the line
-    li $a2, 1                # Maximum number of bytes to read
+    li $a2, 1                # number of bytes to read
     syscall
 
     # Check if EOF (end of file)
     beq $v0, $zero, donereading     # If $v0 is 0, we have reached the end of the file
 
-    lb $t1, 0($a1)
+    lb $t1, 0($a1) #get the value of the byte being read from the buffer 
 
-    sb $t1, line($s1)
-    addi $s1, $s1, 1
-    beq $t1, $s0, processing
+    #
+    # can lb and sb line be shortened
+    #
 
-    j read_loop
+    sb $t1, line($s1) #add this byte to the end of the line
+    addi $s1, $s1, 1 #add 1 to the value of the loop to know which value to get
+    beq $t1, $s0, processing #if a newline character is found, start processing that line
+
+    j read_loop #run this loop again to add onto the line string
 
 processing:
-    addi $t6, $t6, 1
-    move $t1, $s1
-    li $s1, 0
+    addi $t6, $t6, 1 #add 1 to the line counter
+    move $t1, $s1 #store the new value of the length of the line into $t1
+    li $s1, 0 
 
     move $s7, $s6 # $s7 = char position to write from
     add $s6, $s6, $t1 # s6 = number of chars total, $t1 = number of chars to write
@@ -95,82 +103,113 @@ processing:
 
     j linetoint
 
+#CHECK IF THIS VERSION IS VIABLE BEFORE COMMENTING
+processingalt:
+    addi $t6, $t6, 1 #add 1 to the line counter
+    
+    move $s7, $s6 # $s7 = char position to write from
+    add $s6, $s6, $s1 # s6 = number of chars total, $t1 = number of chars to write
+    sw $s1, numcharstowrite
+
+    li $s1, 0 
+    li $s2, 0
+    li $s3, 0
+
+    ble $t6, 4, storefirstfour
+
+    j linetoint
+
 P3toP2:
-    addi $s7, $s7, -2
+    addi $s7, $s7, -2 #go back 2 bytes from the newline character to the number after P
     li $t2, '2'
-    sb $t2, writestring($s7)
-    j rs
+    sb $t2, writestring($s7) #store the number 2 in that space instead of the 3
+    j rs #reset the line space
 
 storenewdescription:
-    addi $s7, $s7, -2
-    lb $t2, writestring($s7)
-    addi $s7, $s7, -1
-    sb $t2, writestring($s7)
+    addi $s7, $s7, -2 #go back 2 bytes and get the last char of the line
+    lb $t1, writestring($s7) 
+
     li $t2, '2'
-    addi $s7, $s7, 1
-    sb $t2, writestring($s7)
-    j rs
+    sb $t2, writestring($s7) #store a number 2 in that place
+
+    addi $s7, $s7, -1 #go back 1 more byte
+    sb $t1, writestring($s7) #set that byte to the same byte as the one that the char at the end initially was
+    #ie: hse become he2, jet become jt2, or tre becomes te2
+    
+    j rs #jump to resetting the 'space' variables
 
 setstart:
-    move $t3, $s6
-    addi $t3, $t3, -1
-    j rs
+    move $t3, $s6 #sets $t3 to the place where the program needs to start adding to the writestring from, ie, after the decription lines
+    addi $t3, $t3, -1 #go back one value so that it getss written right up to the newline vairable
+    j rs #jump to resetting the 'space' variables
 
+#loops through each line of the description strings and writes them to the writing string
 storefirstfour:
     lb $t2, line($s2)
     sb $t2, writestring($s7)
 
-    beq $s7, $s6, resetspaces
+    beq $s7, $s6, resetspaces #if all chars have been written, reset the 'space' variables
     addi $s7, $s7, 1
     addi $s2, $s2, 1
     j storefirstfour
 
+#converting lines (as strings) to integers for calculations
 linetoint:
-    lb  $t1, line($s3)
+    lb  $t1, line($s3) #get the variable of the char
 
-    addi $s3, $s3, 1
+    addi $s3, $s3, 1 #add 1 to the loop for the next char
 
-    beq $t1, $s0, addforgrey
+    beq $t1, $s0, addforgrey #if at end of line, jump to the next step, adding up to calculate the average
 
-    sub $t1, $t1, $s4
-    mul $s2, $s2, $s0
-    add $s2, $s2, $t1
+    sub $t1, $t1, $s4 #get the int value by subbing the char value of '0', which equals 48
+    #ALT VERSION
+    #sub $t1, $t1, '0'
+    mul $s2, $s2, $s0 #timsing $s2 by 10
+    add $s2, $s2, $t1 #adding the value of the integer to the end of $s2
 
-    j linetoint
+    j linetoint #restarting the loop
 
+#adds up the values of the 3 pixels
 addforgrey:
-    addi $t5, $t5, 1
-    add $s8, $s8, $s2
-    beq $t5, 3, converttogrey
-    j read_loop
+    addi $t5, $t5, 1 #add 1 to the counter to check when it's time to calculate the average of the pixels
+    add $s8, $s8, $s2 #add the new number to $s8 
+    beq $t5, 3, converttogrey 
+    #
+    # should be j resetspaces???
+    #
+    j read_loop #jumps back to the read loop to calculate the new numbers (wrong i think)
 
+#gets the average pixel value
 converttogrey:
-    div $s8, $t5
-    mflo $s2
+    #divides the value of the 3 pixels added up by 3 to get the average
+    div $s8, $t5 #divides the total value of the 3 pixels by 3
+    mflo $s2 #stores the average of the 3 pixels in $s2
 
-    li $t5, 0
-    li $s8, 0
+    li $t5, 0 #reset the average counter
+    li $s8, 0 #reset the average number storage
     j numtonewstr
 
+#setting up variables to get the length of the new number
 numtonewstr:  
-    li $t2, 0
-    move $s5, $s2
+    li $t2, 0 #set incrementer to 0
+    move $s5, $s2 #store number in $s5 for calculating down in a loop without losing $s2
     j getnumlen
 
+#getting length of new number (the average pixel value)
 getnumlen:
-    div $s5, $s0
+    div $s5, $s0 #dividing by 10 till the value of the division is 0
     mflo $s5
 
     addi $t2, $t2, 1 #new length stored in $t2
-    beq $s5, $zero, assignnl
+    beq $s5, $zero, assignnl #jumps to assign new length function
     j getnumlen
 
 assignnl:
-    addi $t2, $t2, 1
-    move $t4, $t3
-    add $t3, $t2, $t3
-    move $t2, $t3
-    sb $s0, writestring($t2)
+    addi $t2, $t2, 1 #increment counter by 1 for storing newline char
+    move $t4, $t3 #store countdown value into $t4
+    add $t3, $t2, $t3 #increment $t3 by the number calculated as $t2, which is the number length
+    move $t2, $t3 #store the new number to count down from in $t2 so $t3 remains the main counter, and we count down from $t4 to $t2
+    sb $s0, writestring($t2) #store the newline character at the end of where the string will go
     j newnumtostr
 
 newnumtostr:
@@ -211,23 +250,23 @@ donereading:
     j createnewfile
 
 createnewfile:
-    li   $v0, 13       # system call for open file
+    li   $v0, 13       # system call for closing file
     la   $a0, filenamewrite     # output file name
-    li   $a1, 0x41        # Open for writing (flags are 0: read, W/A: write)
+    li   $a1, 0x41        # Open for writing to file
     syscall
 
     move $t0, $v0            # Store the file descriptor in $t0
 
-    blt $v0, $zero, error
+    blt $v0, $zero, error #show error if there's an error writing to file
 
-    li $v0, 4               # Syscall code for close file
-    la $a0, filesuccess            # File descriptor to close
+    li $v0, 4               # Syscall code printing string
+    la $a0, filesuccess            # show that writing new information to file was a success
     syscall
 
     j writing
 
 writing:
-    li $v0, 15
+    li $v0, 15 
     move $a0, $t0
     la $a1, writestring
     move $a2, $s6
